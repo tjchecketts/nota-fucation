0e75aa0c6a507a632dc4340c95b084af812ea500
Neal Benner <nbenner@youniqueproducts.com>
2019-07-17 20:24:51 -0600

    switched toast progress bars to update every 40 ms so we can have an optional progress bar

--- toast.js
+++ toast.js
@@ -2,7 +2,9 @@ const template = `
 <div class='notaToast'>
     <span class='notaText'></span>
     <button id='notaClose'>X</button>
+    <progress id='notaProgressBar' max=1 value=1></progress>
 </div>
+<style> @import "toast.css"; </style>
 `;
 
 customElements.define('nota-toast', class NotaToast extends HTMLElement {
@@ -12,14 +14,20 @@ customElements.define('nota-toast', class NotaToast extends HTMLElement {
     this.addEventListener('change', this.redraw, {passive: true});
     let shadow = this.attachShadow({mode:'open'});
     Array.from((new DOMParser()).parseFromString(template, 'text/html').body.children).forEach(child => shadow.appendChild(child));
+
     this.messageSpan = shadow.querySelector('.notaText');
+    this.selfDestructProgressBar = shadow.getElementById('notaProgressBar')
 
     shadow.getElementById('notaClose').addEventListener('click', this.destroy.bind(this));
 
     this.startTime = Date.now();
-    this.maxTime = 5000;
+    this.maxTime = 0;
+    this.hovering = false;
+    this.selfDestructCounter = 0;
 
-    this.currentTimeout = setTimeout(this.destroy.bind(this), this.maxTime);
+    if (this.maxTime > 0) {
+      this.selfDestructInterval = setInterval(this.updateSelfDestruct.bind(this), 40);
+    }
 
     this.addEventListener('mouseover', this.mouseOver);
     this.addEventListener('mouseout', this.mouseOut);
@@ -45,24 +53,29 @@ customElements.define('nota-toast', class NotaToast extends HTMLElement {
 
   redraw() {
     this.messageSpan.innerText = this.message;
+    this.selfDestructProgressBar.value = 1 - (this.selfDestructCounter / this.maxTime);
   }
 
   mouseOver() {
-    if (this.currentTimeout) {
-      clearTimeout(this.currentTimeout);
-      this.currentTimeout = null;
-      this.maxTime = this.maxTime - (Date.now() - this.startTime);
-    }
+    this.hovering = true;
   }
 
   mouseOut() {
-    if (!this.currentTimeout) {
-      this.startTime = Date.now();
-      this.currentTimeout = setTimeout(this.destroy.bind(this), this.maxTime);
+    this.hovering = false;
+  }
+
+  updateSelfDestruct() {
+    if (!this.hovering) {
+      this.selfDestructCounter += 40;
+      this.dispatchEvent(new Event('change'));
+    }
+    if (this.selfDestructCounter >= this.maxTime) {
+      this.destroy();
     }
   }
 
   destroy() {
+    clearInterval(this.selfDestructInterval);
     this.parentNode.removeChild(this);
   }
 });
