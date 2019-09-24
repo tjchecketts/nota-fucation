bd6464d3db2163b6d18252eab5834d7bf58b9029
Brett Adamson <adamsonb12@gmail.com>
2019-09-23 19:40:59 -0600

    normalized our indentaiton, fixed the imports and tested in react example

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const url = new URL(import.meta.url);
+
+let pathList = url.pathname.split("/");
+pathList.splice(pathList.length - 1, 1);
+pathList = pathList.join("/");
+
+const notaCssPath = `${pathList}/index.css`;
+const notaToastPath = `${pathList}/toast.js`;
+
 // HTML goes here
 // templates MUST go after the divs
-import './toast.js';
+import(notaToastPath);
 
 const template = `
 <div id='top-left'></div>
@@ -13,75 +22,73 @@ const template = `
     <div id='bottom-middle'></div>
 </div>
 <div id='bottom-right'></div>
-<style> @import "./index.css"; </style>
+<style> @import "${notaCssPath}"; </style>
 `;
 
-customElements.define('nota-fucation', class NotaFucation extends HTMLElement {
-    constructor() {
-        super();
-        let shadow = this.attachShadow({mode:'open'});
-        Array.from((new DOMParser()).parseFromString(template, 'text/html').body.children).forEach(child => shadow.appendChild(child));
-        this.topLeft = shadow.getElementById('top-left');
-        this.topMiddle = shadow.getElementById('top-middle');
-        this.topRight = shadow.getElementById('top-right');
-        this.bottomLeft = shadow.getElementById('bottom-left');
-        this.bottomMiddle = shadow.getElementById('bottom-middle');
-        this.bottomRight = shadow.getElementById('bottom-right');
-        this.notaToastTemplate = shadow.getElementById('nota-toast-template');
-        this.notaDialogTemplate = shadow.getElementById('nota-dialog-template');
-        document.addEventListener('notaToast', this.handleNotaToast.bind(this));
-    }
-
-    handleNotaToast(event) {
-        event.stopPropagation();
-        let thereBeToast = document.createElement('nota-toast');
-        
-        if (!event.message) {
-            console.error('no message provided');
-            // TODO - put back in when ready
-            // return;
+customElements.define(
+    "nota-fucation",
+    class NotaFucation extends HTMLElement {
+        constructor() {
+            super();
+            let shadow = this.attachShadow({ mode: "open" });
+            Array.from(
+                new DOMParser().parseFromString(template, "text/html").body.children
+            ).forEach(child => shadow.appendChild(child));
+            this.topLeft = shadow.getElementById("top-left");
+            this.topMiddle = shadow.getElementById("top-middle");
+            this.topRight = shadow.getElementById("top-right");
+            this.bottomLeft = shadow.getElementById("bottom-left");
+            this.bottomMiddle = shadow.getElementById("bottom-middle");
+            this.bottomRight = shadow.getElementById("bottom-right");
+            this.notaToastTemplate = shadow.getElementById("nota-toast-template");
+            this.notaDialogTemplate = shadow.getElementById("nota-dialog-template");
+            document.addEventListener("notaToast", this.handleNotaToast.bind(this));
         }
 
-        thereBeToast.setAttribute('message', event.message || 'default message');
-        if (event.hasOwnProperty('hideCloseButton')) {
-            thereBeToast.setAttribute('hide-close-button', 'true');
-        }
-        if (event.maxTime) {
-            thereBeToast.setAttribute('max-time', event.maxTime);
-        }
+        handleNotaToast(event) {
+            event.stopPropagation();
+            let thereBeToast = document.createElement("nota-toast");
 
-        const notaClassList = [
-            'danger',
-            'warn',
-            'info',
-            'error',
-            'success',
-            'failure',
-        ];
+            if (!event.message) {
+                console.error("no message provided");
+                // TODO - put back in when ready
+                // return;
+            }
 
-        if (notaClassList.includes(event.class)) {
-            thereBeToast.classList.add(event.class);
-        }
+            thereBeToast.setAttribute("message", event.message || "default message");
+            if (event.hasOwnProperty("hideCloseButton")) {
+                thereBeToast.setAttribute("hide-close-button", "true");
+            }
+            if (event.maxTime) {
+                thereBeToast.setAttribute("max-time", event.maxTime);
+            }
 
-        switch (event.position) {
-            case 'top-left':
-                this.topLeft.appendChild(thereBeToast);
-                break;
-            case 'top-middle':
-                this.topMiddle.appendChild(thereBeToast);
-                break;
-            case 'top-right':
-                this.topRight.appendChild(thereBeToast);
-                break;
-            case 'bottom-left':
-                this.bottomLeft.appendChild(thereBeToast);
-                break;
-            case 'bottom-middle':
-                this.bottomMiddle.appendChild(thereBeToast);
-                break;
-            case 'bottom-right':
-            default:
-                this.bottomRight.appendChild(thereBeToast);
+            const notaClassList = ["danger", "warn", "info", "error", "success", "failure"];
+
+            if (notaClassList.includes(event.class)) {
+                thereBeToast.classList.add(event.class);
+            }
+
+            switch (event.position) {
+                case "top-left":
+                    this.topLeft.appendChild(thereBeToast);
+                    break;
+                case "top-middle":
+                    this.topMiddle.appendChild(thereBeToast);
+                    break;
+                case "top-right":
+                    this.topRight.appendChild(thereBeToast);
+                    break;
+                case "bottom-left":
+                    this.bottomLeft.appendChild(thereBeToast);
+                    break;
+                case "bottom-middle":
+                    this.bottomMiddle.appendChild(thereBeToast);
+                    break;
+                case "bottom-right":
+                default:
+                    this.bottomRight.appendChild(thereBeToast);
+            }
         }
     }
-});
+);
--- toast.js
+++ toast.js
@@ -1,114 +1,122 @@
+const url = new URL(import.meta.url);
+
+let pathList = url.pathname.split("/");
+pathList.splice(pathList.length - 1, 1);
+pathList = pathList.join("/");
+
+const notaCssPath = `${pathList}/toast.css`;
+
 const template = `
-  <span class='notaText'></span>
-  <progress id='notaProgressBar' max=1 value=1></progress>
-  <button id='notaClose'>X</button>
-  <style> @import "./toast.css"; </style>
+<span class='notaText'></span>
+<progress id='notaProgressBar' max=1 value=1></progress>
+<button id='notaClose'>X</button>
+<style> @import "${notaCssPath}"; </style>
 `;
 
-customElements.define('nota-toast', class NotaToast extends HTMLElement {
-  constructor() {
-    super();
-    this.message = this.getAttribute('message');
-    this.hideCloseButton = this.hasAttribute('hide-close-button');
-    this.addEventListener('change', this.redraw, {passive: true});
-    let shadow = this.attachShadow({mode:'open'});
-    Array.from((new DOMParser()).parseFromString(template, 'text/html').body.children).forEach(child => shadow.appendChild(child));
+customElements.define(
+    "nota-toast",
+    class NotaToast extends HTMLElement {
+        constructor() {
+            super();
+            this.message = this.getAttribute("message");
+            this.hideCloseButton = this.hasAttribute("hide-close-button");
+            this.addEventListener("change", this.redraw, { passive: true });
+            let shadow = this.attachShadow({ mode: "open" });
+            Array.from(
+                new DOMParser().parseFromString(template, "text/html").body.children
+            ).forEach(child => shadow.appendChild(child));
 
-    this.messageSpan = shadow.querySelector('.notaText');
-    this.selfDestructProgressBar = shadow.getElementById('notaProgressBar')
+            this.messageSpan = shadow.querySelector(".notaText");
+            this.selfDestructProgressBar = shadow.getElementById("notaProgressBar");
 
-    this.notaClose = shadow.getElementById('notaClose');
-    this.notaClose.addEventListener('click', this.destroy.bind(this));
+            this.notaClose = shadow.getElementById("notaClose");
+            this.notaClose.addEventListener("click", this.destroy.bind(this));
 
-    this.startTime = Date.now();
-    this.maxTime = parseInt(this.getAttribute('max-time') || 7000);
-    this.hovering = false;
-    this.selfDestructCounter = 0;
+            this.startTime = Date.now();
+            this.maxTime = parseInt(this.getAttribute("max-time") || 7000);
+            this.hovering = false;
+            this.selfDestructCounter = 0;
 
-    //TODO if index element - nota-fucation
-      // local vs global
-    //TODO add optional selfDestruct
-      // if it doesnt selfDestruct, then there should be no progress bar
-    //TODO add toggle progress bar
-      // hides progress bar
-      // change the constant checking every 40 ms
-    //TODO add toggle pause on hover
-    //TODO max limit/queueing system for excessive nota-fucations
-    //TODO with queue: persistent to Local storage when marked as persisten/important
-    // TODO => Library specific interfaces for js frameworks (React etc)
+            //TODO if index element - nota-fucation
+            // local vs global
+            //TODO add optional selfDestruct
+            // if it doesnt selfDestruct, then there should be no progress bar
+            //TODO add toggle progress bar
+            // hides progress bar
+            // change the constant checking every 40 ms
+            //TODO add toggle pause on hover
+            //TODO max limit/queueing system for excessive nota-fucations
+            //TODO with queue: persistent to Local storage when marked as persisten/important
+            // TODO => Library specific interfaces for js frameworks (React etc)
 
+            if (this.maxTime > 0) {
+                this.selfDestructInterval = setInterval(this.updateSelfDestruct.bind(this), 40);
+            }
 
-    if (this.maxTime > 0) {
-      this.selfDestructInterval = setInterval(this.updateSelfDestruct.bind(this), 40);
+            this.addEventListener("mouseover", this.mouseOver);
+            this.addEventListener("mouseout", this.mouseOut);
+        }
+
+        static get observedAttributes() {
+            return ["message", "hide-close-button", "max-time"];
+        }
+
+        attributeChangedCallback(name, oldVal, newVal) {
+            let changed = false;
+            if (name === "message" && oldVal !== newVal) {
+                this.message = newVal;
+                changed = true;
+            }
+
+            if (name === "hide-close-button" && oldVal !== newVal) {
+                this.hideCloseButton = this.hasAttribute("hide-close-button");
+                changed = true;
+            }
+
+            if (name === "max-time" && oldVal !== newVal) {
+                this.maxTime = parseInt(this.getAttribute("max-time"));
+                changed = true;
+            }
+
+            if (changed) {
+                this.dispatchEvent(new Event("change"));
+            }
+        }
+
+        connectedCallback() {}
+
+        redraw() {
+            this.messageSpan.innerText = this.message;
+            // notaShower = should be hidden
+            if (this.notaClose.classList.contains("notaShower") && !this.hideCloseButton) {
+                this.notaClose.classList.remove("notaShower");
+            } else if (!this.notaClose.classList.contains("notaShower") && this.hideCloseButton) {
+                this.notaClose.classList.add("notaShower");
+            }
+            this.selfDestructProgressBar.value = 1 - this.selfDestructCounter / this.maxTime;
+        }
+
+        mouseOver() {
+            this.hovering = true;
+        }
+
+        mouseOut() {
+            this.hovering = false;
+        }
+
+        updateSelfDestruct() {
+            if (!this.hovering) {
+                this.selfDestructCounter += 40;
+                this.dispatchEvent(new Event("change"));
+            }
+            if (this.selfDestructCounter >= this.maxTime) {
+                this.destroy();
+            }
+        }
+
+        destroy() {
+            clearInterval(this.selfDestructInterval);
+            this.parentNode.removeChild(this);
+        }
     }
-
-    this.addEventListener('mouseover', this.mouseOver);
-    this.addEventListener('mouseout', this.mouseOut);
-  }
-
-  static get observedAttributes() { 
-    return [
-      'message', 'hide-close-button', 'max-time'
-    ];
-  }
-
-  attributeChangedCallback(name, oldVal, newVal) {
-    let changed = false;
-    if (name === 'message' && oldVal !== newVal) {
-      this.message = newVal;
-      changed = true;
-    }
-
-    if (name === 'hide-close-button' && oldVal !== newVal) {
-      this.hideCloseButton = this.hasAttribute('hide-close-button');
-      changed = true;
-    }
-
-    if (name === 'max-time' && oldVal !== newVal) {
-      this.maxTime = parseInt(this.getAttribute('max-time'));
-      changed = true;
-    }
-
-    if (changed) {
-      this.dispatchEvent(new Event('change'));
-    }
-  }
-
-  connectedCallback() {
-
-  }
-
-  redraw() {
-    this.messageSpan.innerText = this.message;
-    // notaShower = should be hidden
-    if (this.notaClose.classList.contains('notaShower') && !this.hideCloseButton) {
-      this.notaClose.classList.remove('notaShower');
-    } else if (!this.notaClose.classList.contains('notaShower') && this.hideCloseButton) {
-      this.notaClose.classList.add('notaShower');
-    }
-    this.selfDestructProgressBar.value = 1 - (this.selfDestructCounter / this.maxTime);
-  }
-
-  mouseOver() {
-    this.hovering = true;
-  }
-
-  mouseOut() {
-    this.hovering = false;
-  }
-
-  updateSelfDestruct() {
-    if (!this.hovering) {
-      this.selfDestructCounter += 40;
-      this.dispatchEvent(new Event('change'));
-    }
-    if (this.selfDestructCounter >= this.maxTime) {
-      this.destroy();
-    }
-  }
-
-  destroy() {
-    clearInterval(this.selfDestructInterval);
-    this.parentNode.removeChild(this);
-  }
-});
+);
